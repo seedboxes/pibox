42fe04d818eed37e24b3aa48b04116c9d0af2ba8
Hadopibox Admin <hadopibox@gmail.com>
2015-09-16 11:00:38 +0200

    replace apache2 with nginx

--- go.sh
+++ go.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/bash -x
 
 #RTORRENT_DEFAULT="/opt/rtorrent"
 p="${RTORRENT_VOLUME:-$RTORRENT_DEFAULT}"
 
 echo " ==> Init rutorrent folder"
 mkdir -p $p/{session,share,watch}
-htpasswd -bc $p/.htpasswd ${PIBOX_USER:-"hadopi"} ${PIBOX_PASS:-"fuckyou"}
+#htpasswd -bc $p/.htpasswd ${PIBOX_USER:-"hadopi"} ${PIBOX_PASS:-"fuckyou"}
 
 # force unlock
 rm -f $p/session/rtorrent.lock
@@ -17,9 +17,8 @@ then
   openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $p/ssl.key -out $p/ssl.crt -batch
 fi
 
-# apache setup
-sed -i "s,$RTORRENT_DEFAULT,$p,g" /etc/apache2/sites-enabled/rutorrent.conf
 sed -i "s,$RTORRENT_DEFAULT,$p,g" /root/.rtorrent.rc
 
-#source /etc/apache2/envvars && exec /usr/sbin/apache2 -DFOREGROUND
+/etc/init.d/php5-fpm start
+/etc/init.d/nginx start
 supervisord -n
