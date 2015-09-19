48dfd33f5ffe7ab0cf958f215cada127ffdbb4e0
hadopi <hadopibox@gmail.com>
2015-09-19 04:06:37 +0200

    enhance container startup

--- src/go.sh
+++ src/go.sh
@@ -1,19 +1,85 @@
-#!/bin/bash -x
+#!/bin/bash
+
+   rst="$(tput sgr0)"
+   bld="$(tput bold)"
+   und="$(tput sgr 0 1)"
+
+   red="$(tput setaf 1)"
+ green="$(tput setaf 2)"
+yellow="$(tput setaf 3)"
+ white="$(tput setaf 7)"
+
+bgreen="${bld}${green}"
+bwhite="${bld}${white}"
+
+[ ! -z "$PIBOX_DEBUG" ] && set -x
+
+RESULT=0
+function show_result {
+    if [ $? -eq 0 ]
+    then
+        echo "${green}[SUCCESS]${rst}"
+    else
+        echo "${red}[FAILURE]${rst}"
+        RESULT=$(( RESULT + 1))
+    fi
+}
+
+echo "
+                        ${bgreen} __     __   __      ${rst}
+                        ${bgreen}|__) | |__) /  \ \_/ ${rst}
+                        ${bgreen}|    | |__) \__/ / \ ${rst}
+"
 
 #RTORRENT_DEFAULT is defined in the Dockerfile to "/opt/rtorrent"
+user="${PIBOX_USER:-"hadopi"}"
+pass="${PIBOX_PASS:-"fuckyou"}"
 p="${RTORRENT_VOLUME:-$RTORRENT_DEFAULT}"
+sed -i "s,$RTORRENT_DEFAULT,$p,g" /etc/nginx/sites-enabled/rutorrent.conf
+sed -i "s,$RTORRENT_DEFAULT,$p,g" /root/.rtorrent.rc
 
-echo " ==> Init rutorrent folder"
+echo
+echo "$bwhite ==> RUTORRENT setup$rst"
+
 mkdir -p $p/{session,share,watch}
+rm -f $p/session/rtorrent.lock # force unlock
 
-printf "${PIBOX_USER:-"hadopi"}:$(openssl passwd -crypt ${PIBOX_PASS:-"fuckyou"})\n" >> $p/.htpasswd
+echo
+echo "$bwhite ==> CREDENTIALS$rst"
 
-# force unlock
-rm -f $p/session/rtorrent.lock
+pwdfile="$p/.htpasswd"
+if [ ! -f "$pwdfile" ]
+then
+    echo -n "   > Setting up username / password for WEB access... "
+    printf "${user}:$(openssl passwd -crypt "${pass}")\n" >> "${pwdfile}"
+    show_result $?
+    echo "   >    username: $user"
+    echo "   >    password: $pass"
+else
+    echo "   > A password file already exists... [SKIPPING]"
+fi
 
-# ssl setup
+if [ ! -z "${PIBOX_FTP}" ] && [ "${PIBOX_FTP}" = "yes" ]
+then
+    echo -n "   > Setting up username/password for FTP access... "
+    echo -e "${pass}\n${pass}" > /tmp/passin
+    pure-pw useradd "$user" -d "$p/share" -u ftpuser -m < /tmp/passin 2>&1 >/dev/null && pure-pw mkdb
+    show_result $?
+
+    rm /tmp/passin
+    sed -i "s,PIBOX_PUBLICIP,${PIBOX_PUBLICIP}," /etc/supervisor/conf.d/ftp.conf
+else
+    echo -n "   > Desactivating FTP access... "
+    mv /etc/supervisor/conf.d/ftp.{conf,inactive}
+    show_result $?
+fi
+
+echo
+echo "$bwhite ==> SSL$rst"
+
 if [[ ! -e $p/ssl.key ]] || [[ ! -e $p/ssl.crt ]]
 then
+    echo -n "   > Creating SSL certificate and key files... "
     # the generated certificate is also a self-signed CA and can be added to you Trusted CA 
     # in order to get a "green address bar" in your browser and avoid the ssl warning
     openssl x509 \
@@ -29,28 +95,30 @@ then
         -days 3650 \
         -extfile <(echo -e "basicConstraints=critical,CA:true,pathlen:0") \
         -out "$p/ssl.crt"
+    show_result $?
 
     chmod 400 $p/ssl.key
-fi
-
-sed -i "s,$RTORRENT_DEFAULT,$p,g" /etc/nginx/sites-enabled/rutorrent.conf
-sed -i "s,$RTORRENT_DEFAULT,$p,g" /root/.rtorrent.rc
-
-/etc/init.d/php5-fpm start
-/etc/init.d/nginx start
-
-#=== FTP ===#
-
-if [ ! -z "${PIBOX_FTP}" ] && [ "${PIBOX_FTP}" = "yes" ]
-then
-    echo "${PIBOX_PASS:-"fuckyou"}" >  /tmp/in 
-    echo "${PIBOX_PASS:-"fuckyou"}" >> /tmp/in 
-    pure-pw useradd "${PIBOX_USER:-"hadopi"}" -d "$p/share" -u ftpuser -m < /tmp/in
-    pure-pw mkdb
-    
-    sed -i "s,PIBOX_PUBLICIP,${PIBOX_PUBLICIP}," /etc/supervisor/conf.d/ftp.conf
 else
-    mv /etc/supervisor/conf.d/ftp.{conf,inactive}
+    echo "   > A certificate file already exists... [SKIPPING]"
 fi
 
-supervisord -n
+echo
+echo "$bwhite ==> SERVICES$rst"
+
+echo -n "   > Starting php... "
+/etc/init.d/php5-fpm start
+show_result $?
+
+echo -n "   > Starting http server... "
+/etc/init.d/nginx start
+show_result $?
+
+echo
+if [ $RESULT -eq 0 ]
+then
+  echo "$bgreen ==> PIBOX STARTED SUCCESSFULLY$rst"
+  supervisord -n -e error -c /etc/supervisor/supervisord.conf
+else
+  echo "$bred ==> PIBOX FAILED TO START :("
+  echo "$red   > check above failure and ask for help if needed: https://github.com/seedboxes/pibox/issues"
+fi
